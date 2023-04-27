5ecf126c4ed306af1088189a45754cb8e92ad272
Federica Pozzer <federica.pozzer@gmail.com>
2023-04-27 14:49:26 +0200

    prettier cards

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,14 +2,14 @@
 
 export default {
     name: "Project card",
-    
+
     props: {
         project: Object,
     },
 
     computed: {
         abstract() {
-            return this.project.text.slice(0, 50) + ".."; //da dove parto, quanti caratteri voglio
+            return this.project.text.slice(0, 100) + ".."; //da dove parto, quanti caratteri voglio
         }
     },
 }
@@ -22,21 +22,12 @@ export default {
     <div class="col d-flex">
         <div class="card w-100">
             <div class="card-header d-flex overflow-auto justify-content-between">
-
                 <!-- pill (il div esterno è perchè se no non resta a forma di pill, diventa tonda) -->
                 <div>
                     <div class="badge rounded-pill me-3" :style="{ 'background-color': project.type.color }">
                     {{ project.type.name }}
                     </div>
                 </div>
-
-                <!-- technologies -->
-                <div class="d-flex ms-2" v-if="project.technologies.length">Technology:
-                    <div class="badge ms-1" v-for="technology in project.technologies" :key="technology.id" :style="{ 'background-color': technology.color }">
-                        {{ technology.name }}                 
-                    </div>
-                </div>
-                
             </div>
 
             <div class="card-body">
@@ -45,6 +36,15 @@ export default {
                 <p class="card-text"> <small> {{ abstract }} </small> </p>
                 <!-- * BUTTON CHE SERVIRA' PER LA SHOW
                     <a href="" class="btn btn-primary">See Project</a> -->
+            </div>
+
+            <div class="card-footer" v-if="project.technologies.length">
+                <!-- technologies -->
+                <div class="d-flex justify-content-end" >Tech:
+                    <div class="badge ms-1" v-for="technology in project.technologies" :key="technology.id" :style="{ 'background-color': technology.color }">
+                        {{ technology.name }}                 
+                    </div>
+                </div>
             </div>
         </div>
     </div>
